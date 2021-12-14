298bb91c979c84c0ba74efe7b9c7a3310ee42ef6
dinesh1912 <dineshdini1912@gmail.com>
2021-12-14 17:52:46 +0530

    she is okay

---
+++ src/org/sri/SrimathiCried.java
@@ -0,0 +1,12 @@
+package org.sri;
+
+public class SrimathiCried {
+	public static void main(String[] args) {
+		String s1="java";
+		String s2="application";
+		String concat = s1.concat(s2);
+		System.out.println(concat.substring(s1.length()));
+		System.out.println(concat.substring(0, s1.length()));
+	}
+
+}
